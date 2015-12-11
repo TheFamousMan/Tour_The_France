3448f6e1bb2c8776e3bfe6930f4ecca5acbc9eb7
Natan143 <Natanek@mbp-natanek.media.local>
2015-12-11 10:12:35 +0100

    gefixd home & database

--- index.php
+++ index.php
@@ -29,17 +29,38 @@ require_once 'library/database.php';
             
             while ($song = $result->fetch_assoc()) {
             
+
             echo '<div id="article">';
-            echo '<div id="placeholder"><iframe src="https://www.youtube.com/embed/'.$song['yt-link'].'" frameborder="0" allowfullscreen></iframe></div>';
-            echo '<div id="articleTitle"><p id="numberSong">'.$song['rank'].'</p><p id="title">'.$song['artist'].' - '.$song['song'].'</p></div>';
-            echo '<div id="articleDes"><p>Lorem ipsumashdfvausdvfasd dsafb asdhbfsaydb fuyasbdfi.</p></div>';
-            echo '<div id="articleSocial"></div>';
+                echo '<div class="articleL">';
+                echo '<div id="placeholder"><iframe src="https://www.youtube.com/embed/'.$song['yt-link'].'" frameborder="0" allowfullscreen></iframe></div>';
+                echo '</div>';
+                echo '<div class="articleR">';
+                echo '<div id="articleTitle"><p id="numberSong">'.$song['rank'].'</p><p id="title">'.utf8_encode($song['artist']).' : '.utf8_encode($song['song']).'</p></div>';
+                echo '<div id="articleDes"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit.</p></div>';
+                echo '<div id="articleSocial"><div class="talk"><img src="icons/talk.png"></div><a> [999]</a><a> | </a><a>Lees meer ➡</a><img src="icons/mail.png"><img src="icons/twt.png"><img src="icons/fb.png"></div>';
+                echo '</div>';    
             echo '</div>';
-        }
-            ?>
-                    
-                    
+            }
+            ?>    
                 
             </div>
         </div>
     </body>
+</html>
+<?php 
+
+$main = (empty($_GET['main'])) ? '' : $_GET['main'];
+
+switch ($main) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+            
+        break;
+}
+?>
